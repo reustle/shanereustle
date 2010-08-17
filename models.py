63cd1a4e733cabaa7eb84efb2cb4e848138a2cb7
Shane Reustle <sreustle@gmail.com>
2010-08-16 23:10:17 -0400

    The correct collection name helps. I must be tired or something

--- models.py
+++ models.py
@@ -17,7 +17,7 @@ class DatabaseClass():
 	
 	def keep_alive(self):
 		try:
-			self.mongohq.shanereustle.content.find_one()
+			self.mongohq.shanereustle.blog.find_one()
 		except (AutoReconnect, OperationFailure):
 			self.authenticate()
 		
