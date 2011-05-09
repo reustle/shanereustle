bdfccea2c622b2068bc232a31bc8e064506eb0db
Shane Reustle <sreustle@gmail.com>
2011-05-08 22:51:45 -0400

    Working on replacing tumblr

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
 from bottle import debug, route, static_file, run, default_app
 import settings
 from controllers.index import *
+from controllers.blog import *
 
 debug(settings.DEBUG)
 
-@route("/static/:filename#.*#")
+@route('/static/:filename#.*#')
 def static(filename):
-	return static_file(filename, root=settings.PROJECT_PATH+"/static")
+	return static_file(filename, root=settings.PROJECT_PATH + '/static')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 	run(reloader=settings.AUTO_RELOAD)
 
