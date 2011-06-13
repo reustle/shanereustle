596a3c050ee87d7f4fda60c6a7c0f4cdadbd4559
Shane Reustle <sreustle@gmail.com>
2011-06-13 16:41:27 -0400

    Added fonts to new static dir, cleaned up basic stuff

--- node/server.js
+++ node/server.js
@@ -15,80 +15,105 @@ function load_template(path, callback){
 	}
 }
 
+function error_handler(err, res){
+	if(err){
+		res.end('ERROR');
+		return true;
+	}
+}
+
 http.createServer(function(req, res){
 	
 	var url = req.url.split('/');
 	
-	// TODO: Match urls with regex
 	if(req.url == '/'){
 		// Match URL: /
 		load_template('templates/index.html', function(err, template){
 			res.writeHead(200, {'Content-Type': 'text/html'});
 			res.end(template + '\n');
 		});
+	
+	}else if( /\/static\//.test(req.url)){
+		// Match URL: /static/
 		
-	}else if(url[1] == 'blog'){
-		if(url.length == 2 || url[2].length < 1){
-			// Match URL: /blog/
-			
-			function callback(){
-				console.log('callme');
-			}
-			
-			async.parallel({
-				'template': function(callback){
-					load_template('templates/blog.html', callback);
-				},
-				'blog_index': function(callback){
-					load_template('templates/blog_index.html', callback);
-				}
-			}, function(err, response){
-				var template = response.template;
-				template = template.replace('{{title}}', 'Blog &#124; Shane Reustle');
-				template = template.replace('{{body}}', response.blog_index);
-				
-				res.writeHead(200, {'Content-Type': 'text/html'});
-				res.end(template + '\n');
-				
-			});
-			
-		}else{
-			// Match URL: /blog/{article}/
-			
-			if(url[2] == 'purge'){
-				template_cache = {};
-				res.writeHead(200, {'Content-Type': 'text/plain'});
-				res.end('Purged Cache\n');
-				return false;
-			}
-			
-			async.parallel({
-				'template': function(callback){
-					load_template('templates/blog.html', callback);
-				},
-				'entry': function(callback){
-					var entry_filename = 'templates/blog_entries/' + url[2] + '.html';
-					load_template(entry_filename, callback);
-				}
-			}, function(err, response){
-				// TODO: This is ugly. Regex maybe?
-				var entry_title = response.entry.split('\n')[0].replace('<h1>', '').replace('</h1>', '');
-				
-				var template = response.template;
-				template = template.replace('{{title}}', entry_title + ' &#124; Shane Reustle');
-				template = template.replace('{{body}}', response.entry);
-				
-				res.writeHead(200, {'Content-Type': 'text/html'});
-				res.end(template + '\n');
-				
-			});
-			
+		types = {
+			'css' : 'text/css',
+			'gif' : 'image/gif',
+			'jpeg' : 'image/jpeg',
+			'jpg' : 'image/jpeg',
+			'js' : 'application/javascript',
+			'png' : 'image/png'
 		}
 		
-	}else if(url[1] == 'favicon.ico'){
+		var filename = req.url.replace(/\//, '');
+		var filetype = filename.split('.');
+		filetype = types[filetype[filetype.length -1]];
+		
+		fs.readFile(filename, 'binary', function(err, data){
+			if(err){ throw err }
+			res.writeHead(200, {'Content-Type': filetype});
+			res.write(data, 'binary');
+			res.end();
+		});
+	
+	}else if( /^\/blog\/?$/.test(req.url)){
+		// Match URL: /blog/
+		
+		async.parallel({
+			'template': function(callback){
+				load_template('templates/blog.html', callback);
+			},
+			'blog_index': function(callback){
+				load_template('templates/blog_index.html', callback);
+			}
+		}, function(err, response){
+			var template = response.template;
+			template = template.replace('{{title}}', 'Blog &#124; Shane Reustle');
+			template = template.replace('{{body}}', response.blog_index);
+			
+			res.writeHead(200, {'Content-Type': 'text/html'});
+			res.end(template + '\n');
+			
+		});
+		
+	}else if( /^\/blog\//.test(req.url)){
+		// Match URL: /blog/{article}/
+		
+		if(url[2] == 'purge'){
+			template_cache = {};
+			res.writeHead(200, {'Content-Type': 'text/plain'});
+			res.end('Purged Cache\n');
+			return false;
+		}
+		
+		async.parallel({
+			'template': function(callback){
+				load_template('templates/blog.html', callback);
+			},
+			'entry': function(callback){
+				var entry_filename = 'templates/blog_entries/' + url[2] + '.html';
+				load_template(entry_filename, callback);
+			}
+		}, function(err, response){
+			if(error_handler(err, res)){return}
+			
+			var entry_title = response.entry.split('\n')[0];
+			entry_title = /<h1>(.+)<\/h1>/.exec(entry_title)[1];
+			
+			var template = response.template;
+			template = template.replace('{{title}}', entry_title + ' &#124; Shane Reustle');
+			template = template.replace('{{body}}', response.entry);
+			
+			res.writeHead(200, {'Content-Type': 'text/html'});
+			res.end(template + '\n');
+			
+		});
+	
+	}else if( /favicon\.ico/.test(req.url)){
 		// Match URL: /favicon.ico
+		
+		// Return empty_gif
 		res.writeHead(200, {'Content-Type': 'image/gif'});
-		// EmptyGif
 		res.end('\x47\x49\x46\x38\x39\x61\x01\x00\x01\x00\xf0\x01\x00\xff\xff\xff\x00\x00\x00\x21\xf9\x04\x01\x0a\x00\x00\x00\x2c\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02\x44\x01\x00\x3b', 'binary');
 		
 	}else{
