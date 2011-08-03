425d5262965f6daafe670c5e49ce11e0fc050f01
Shane Reustle <sreustle@gmail.com>
2011-08-02 23:17:16 -0400

    Back to basics

---
+++ static/js/ga.js
@@ -0,0 +1,10 @@
+var _gaq = [['_setAccount', 'UA-2580539-1'], ['_trackPageview']];
+(function() {
+	var ga = document.createElement('script');
+	ga.type = 'text/javascript';
+	ga.async = true;
+	ga.src = 'http://.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(ga, s);
+})();
+
