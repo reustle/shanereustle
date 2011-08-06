c246e48ba1838b9a33b70285f3441d5feb361f19
Shane Reustle <sreustle@gmail.com>
2011-08-06 15:09:22 -0400

    Fixed GA.js

--- static/js/ga.js
+++ static/js/ga.js
@@ -3,7 +3,7 @@ var _gaq = [['_setAccount', 'UA-2580539-1'], ['_trackPageview']];
 	var ga = document.createElement('script');
 	ga.type = 'text/javascript';
 	ga.async = true;
-	ga.src = 'http://.google-analytics.com/ga.js';
+	ga.src = 'http://google-analytics.com/ga.js';
 	var s = document.getElementsByTagName('script')[0];
 	s.parentNode.insertBefore(ga, s);
 })();
