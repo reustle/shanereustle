240c41a4338b7dc05c3c45a699d15ab2a6b8793c
Shane Reustle <sreustle@gmail.com>
2012-06-02 02:08:49 -0400

    Fixed banner href

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -5,7 +5,7 @@ var _vrq = _vrq || [];
 	var insert_banner = function(){
 		var banner_el = document.createElement('div');
 		var wrapper = document.getElementsByTagName('div')[0];
-		var banner = '<div id="banner"><div class="wrapper"><a href="/blog/">Shane<span class="red">Reustle</span></a><div class="right">';
+		var banner = '<div id="banner"><div class="wrapper"><a href="/">Shane<span class="red">Reustle</span></a><div class="right">';
 		banner += '<a class="icons twitter" title="Twitter" href="http://twitter.com/reustle/"></a>';
 		banner += '<a class="icons github" title="GitHub" href="http://github.com/reustle/"></a>';
 		banner += '<a class="icons linkedin" title="LinkedIn" href="http://linkedin.com/in/reustle/"></a>';
