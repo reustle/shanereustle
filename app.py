24e139eba55113eb8d36a46f162fda892d124d3c
Shane Reustle <me@shanereustle.com>
2013-08-03 21:36:04 +0200

    Redirect everything to reustle.io

---
+++ app.py
@@ -0,0 +1,25 @@
+import pystache
+import json
+from flask import Flask, redirect
+import settings as SETTINGS
+app = Flask(__name__)
+
+# Routing & Controllers
+@app.route('/')
+def blog_index(blog_page=1):
+	""" The list of blog entries """
+	
+	return redirect('http://reustle.io', 301)
+	
+@app.route('/blog/<slug>.html')
+def blog_entry(slug):
+	""" Redirect Blog Entries """
+	
+	if slug == 'freelance-software-engineer':
+		slug = 'freelance-software-developer'
+	
+	return redirect('http://reustle.io/blog/' + slug + '.html', 301)
+
+if __name__ == '__main__':
+	app.run(port=SETTINGS.port, debug=True)
+
