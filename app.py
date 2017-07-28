de4bafe2afa57873ec5d4146b6f6b3f2d4f85600
Shane Reustle <me@shanereustle.com>
2017-07-28 17:17:59 +0900

    fix

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_redirect():
-    redirect('http://reustle.org/', code=301)
+    return redirect('http://reustle.org/', code=301)
 
 @app.route('/blog/')
 def blog_redirect():
-    redirect('http://reustle.org/articles', code=301)
+    return redirect('http://reustle.org/articles', code=301)
 
 @app.route('/blog/<article>')
 def blog_redirect(article):
-    redirect('http://reustle.org/'+article, code=301)
+    return redirect('http://reustle.org/'+article, code=301)
 
